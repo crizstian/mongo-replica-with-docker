e6d9aa74ac09531fe31b255964a61891b09436dc
Cristian <cristiano.rosetti@gmail.com>
2017-01-13 10:46:39 -0600

    step 1 and step 2 added

---
+++ admin.js
@@ -0,0 +1,18 @@
+admin = db.getSiblingDB("admin")
+admin.createUser(
+  {
+    user: "cristian",
+    pwd: "cristianPassword2017",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+  }
+)
+
+db.getSiblingDB("admin").auth("cristian", "cristianPassword2017" )
+
+db.getSiblingDB("admin").createUser(
+  {
+    "user" : "replicaAdmin",
+    "pwd" : "replicaAdminPassword2017",
+    roles: [ { "role" : "clusterAdmin", "db" : "admin" } ]
+  }
+)
---
+++ replica.js
@@ -0,0 +1,6 @@
+rs.initiate({
+ _id: 'rs1',
+ members: [{
+  _id: 0, host: 'manager1:27017'
+ }]
+})
